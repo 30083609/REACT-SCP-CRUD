1d5a78adce0d40b10db9d895f430db64e3ecd49c
30083609 <30083609@student.toiohomai.ac.nz>
2025-05-22 13:28:55 +1200

    Update main.jsx
    Added notes for readability

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles.css'
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
+// Initialize React in the #root element and start the render loop
+ReactDOM.createRoot(
+  document.getElementById('root')    // grab the DOM node with id="root"
+).render(
+  <React.StrictMode>                
+    <App />                         
   </React.StrictMode>
 )
